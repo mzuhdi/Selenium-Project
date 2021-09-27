7e7c13dda57bbfe020c1881d34b07d82d0f959d9
Muhammad Zuhdi <Muhammad.Zuhdi07@gmail.com>
2021-09-26 22:27:09 -0400

    more changes

--- src/test/java/UsingWebDriverManager.java
+++ src/test/java/UsingWebDriverManager.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class UsingWebDriverManager {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class UsingWebDriverManager {
+    @BeforeClass
+    public static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+        WebDriverManager.edgedriver().setup();
+    }
+    @Test
+    public void startTest(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.close();
+    }
+    @Test
+    public void assignmentOne(){
+        WebDriver chromeDriver = new ChromeDriver();
+        WebDriver edgeDriver = new EdgeDriver();
+
+        edgeDriver.get("https://the-internet.herokuapp.com/");
+        chromeDriver.get("https://example.cypress.io/");
+        chromeDriver.get("https://react-shopping-cart-67954.firebaseapp.com/");
+    }
+
+    @Test
+    public void assignmentOneAnswer() throws InterruptedException{
+        WebDriver edgeDriver = new EdgeDriver();
+        edgeDriver.get("https://the-internet.herokuapp.com/");
+        Thread.sleep(3000);
+        edgeDriver.quit();
+    }
+
 }
--- src/test/java/locating/elements/LocatingElementsTest.java
+++ src/test/java/locating/elements/LocatingElementsTest.java
@@ -1,2 +1,34 @@
-package locating.elements;public class LocatingElementsTest {
+package locating.elements;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+//import static org.junit.Assert.assertTrue;
+
+public class LocatingElementsTest {
+    @Test
+    public void elementsQuiz1(){
+
+        //Find the chromedriver
+        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
+        //Instantiate the driver
+        WebDriver driver = new ChromeDriver();
+        //Navigate to url
+        driver.get("https://www.saucedemo.com");
+        //Find element and check state
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("userName")));
+        //take action + record result
+        //assertTrue(element.isDisplayed());
+        //quit the driver
+        driver.quit();
+    }
+
 }
